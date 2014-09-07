737d8ee6fb7165969873b420bce41b3193d6b873
slince <taosikai@sina.cn>
2014-09-07 13:50:36 +0800

    add file

---
+++ src/Container.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * slince depency injection component
+ * @author Taosikai <taosikai@yeah.net>
+ */
+namespace Slince\Di;
+
+class Container
+{
+	function set()
+	{
+		
+	}
+	function get()
+	{
+		
+	}
+	function alias()
+	{
+		
+	}
+	function newInstance()
+	{
+		
+	}
+}
